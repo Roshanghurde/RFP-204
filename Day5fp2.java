2348720a4d1d00cdcec8119a1b5d6154b62284ca
Roshanghurde <111034142+Roshanghurde@users.noreply.github.com>
2022-09-29 16:02:38 +0530

    Add files via upload

---
+++ Day5fp2.java
@@ -0,0 +1,33 @@
+package Day5_problems;
+import java.io.*;
+import java.util.*;
+public class Day5fp2 {
+
+
+       public static void findTriplets(int[] arr, int N)
+		{
+			boolean found = false;
+			for (int i = 0; i < N - 2; i++) {
+				for (int j = i + 1; j < N - 1; j++) {
+					for (int k = j + 1; k < N; k++) {
+						if (arr[i] + arr[j] + arr[k] == 0) {
+							System.out.println(arr[i] + " "
+											+ arr[j] + " "
+											+ arr[k]);
+							found = true;
+						}
+					}
+				}
+			}
+
+			if (found == false)
+				System.out.println(" not exist ");
+		}
+
+		public static void main(String[] args)
+		{
+			int arr[] = { 0, -1, 2, -3, 1 };
+			int n = arr.length;
+			findTriplets(arr, n);
+		}
+	}
